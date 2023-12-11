e7a991aa5fa7883010ef2b4a53b6b49fd062c26c
Massimiliano Demiri <massimilianodemiridev@proton.me>
2023-12-11 11:40:32 +0100

    add

---
+++ js/temp-3.js
@@ -0,0 +1,8 @@
+
+// Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
+
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = ":)"
+
+console.log(`Hi ${name}, ${message}, ${greeting} !`);
