bd013eb9de0b9337586f6b2edfcae811f9f11dd7
Massimiliano Demiri <massimilianodemiridev@proton.me>
2023-12-11 10:32:58 +0100

    add

---
+++ js/es5.js
@@ -0,0 +1,26 @@
+
+// Implementare la funzione ageAverage che, dato un array di persone, calcola l'età media.
+
+function calculateAverageAge(persons) {
+  const sum = persons.reduce((a, person) => a+person.age,0);
+  const avg = sum/persons.length;
+  return avg;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
+
