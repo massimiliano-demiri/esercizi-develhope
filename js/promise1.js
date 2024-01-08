20a969a35869f5213da4fcb96932797f0e03305e
Massimiliano Demiri <massimilianodemiridev@proton.me>
2024-01-08 08:15:38 +0100

    add

---
+++ js/promise1.js
@@ -0,0 +1,26 @@
+
+    // Riscrivi la funzione fetchDataFromAPI dell'esercizio Le Callback - 6 utilizzando Promises per una migliore gestione degli errori.
+    // La promise dovrebbe risolversi con i dati e rifiutare con un messaggio di errore.
+
+    function fetchDataFromAPI() {
+    
+      return new Promise((resolve, reject) =>{
+          setTimeout(() =>{
+              const dati = { name: "John", age: 30 };
+              if (dati){
+                  resolve(dati);
+              } else {
+                  reject("Impossibile il recupero dei dati da API");
+              }
+          }, 1000);
+  
+      });
+    }
+
+  
+    fetchDataFromAPI().then((dati)=>{
+      console.log("successo", dati);
+    })
+    .catch((errore)=>{
+      console.log("errore", errore)
+    });
